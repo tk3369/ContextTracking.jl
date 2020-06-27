8d7dd8d2e991840c4d685f8f7eb5c923b2a6fed7
Tom Kwong <tk3369@gmail.com>
2020-06-27 12:12:11 -0700

    Removed ContextLogger

--- src/ContextTracking.jl
+++ src/ContextTracking.jl
@@ -8,12 +8,10 @@ using ExprTools: combinedef, splitdef
 export Context
 export context, save, restore
 export @ctx, @memo, call_path
-export ContextLogger
 
 include("types.jl")
 include("context.jl")
 include("singleton.jl")
 include("trace.jl")
-include("logger.jl")
 
 end # module
